83684f9524d03fe08e6451e98aa926ed0843074c
8t3 <behindthedankmem@gmail.com>
2017-04-20 21:19:07 -0700

    made congratulations.js

---
+++ congratulations.js
@@ -0,0 +1,6 @@
+// JAVASCRIPT TROLL
+
+window.alert("Congratulations! You qualify for a gift card of your choice! To continue, type in your e-mail address.");
+prompt("What is your e-mail address?")
+window.alert("Thank you! Redirecting to gift card site...");
+window.alert("JUST KIDDING WE JUST STOLE ALL OF YOUR PERSONAL INFORMATION AND PASSWORDS BYE ;)");
