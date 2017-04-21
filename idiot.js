9935023b5f87ed38f84296e04b2079838c85bd48
8t3 <behindthedankmem@gmail.com>
2017-04-20 21:30:22 -0700

    created idiot.js

---
+++ idiot.js
@@ -0,0 +1,5 @@
+// idiot.js, the js that makes you feel like an idiot
+
+window.alert("This is very dangerous... do you want to continue?");
+window.alert("You asked for it...");
+window.open ("http://youareanidiot.org","mywindow","status=1");
